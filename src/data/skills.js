03deda09eddb6b4459e8935cd07c0a7e8b7edef4
Epictrepic <laith.benbelgacem@gmail.com>
2024-05-10 18:57:09 +0200

    added folders + files

---
+++ src/data/skills.js
@@ -0,0 +1,20 @@
+const skills = [
+    {
+        "title": "html",
+        "image": "./media/images/html_logo.png"
+    },
+    {
+        "title": "css",
+        "image": "./media/images/css_logo.png"
+    },
+    {
+        "title" : "js",
+        "image" : "./media/images/js_logo.png"
+    },
+    {
+        "title" : "react",
+        "image" : "./media/images/react_logo.png"
+    }
+];
+
+export default skills;
