c2713d02ee500af6f3db0d34904ee70e07c0d98c
laisbenb <147707118+laisbenb@users.noreply.github.com>
2024-05-18 11:59:39 +0200

    Added clock component display live clock

---
+++ src/components/Clock.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+
+function liveClock() {
+
+    // live clock data
+    const time = new Date();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+
+    // empty variable for storing correct time
+    let setHour;
+    let setMinute;
+
+    // Add '0' before single number
+    if (hour < 10) {
+        setHour = '0' + hour;
+    } else {
+        setHour = hour;
+    }
+    if (minute < 10) {
+        setMinute = '0' + minute;
+    } else {
+        setMinute = minute;
+    }
+
+    return `${setHour}:${setMinute}`;
+};
+
+export default function Clock() {
+    const [time, setTime] = useState(liveClock());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(liveClock());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+  return (
+    <div className='nav__clock'>
+        {time}
+    </div>
+  )
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { ROUTES } from '../routes/routes';
+import Clock from './Clock';
 
 export default function Navigation() {
   return (
     <div className='nav'>
-      <NavLink to={ROUTES.home.path}>{ROUTES.home.title}</NavLink>
-      <NavLink to={ROUTES.about.path}>{ROUTES.about.title}</NavLink>
-      <NavLink to={ROUTES.portfolio.path}>{ROUTES.portfolio.title}</NavLink>
-      <NavLink to={ROUTES.contact.path}>{ROUTES.contact.title}</NavLink>
+      <div className='nav__links'>
+        <NavLink to={ROUTES.home.path}>{ROUTES.home.title}</NavLink>
+        <NavLink to={ROUTES.about.path}>{ROUTES.about.title}</NavLink>
+        <NavLink to={ROUTES.portfolio.path}>{ROUTES.portfolio.title}</NavLink>
+        <NavLink to={ROUTES.contact.path}>{ROUTES.contact.title}</NavLink>
+      </div>
+      <Clock />
     </div>
   );
 }
