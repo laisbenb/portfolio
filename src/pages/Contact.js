8cdf803b3880d8dff0261cf7db9d57e52e773cce
Epictrepic <laith.benbelgacem@gmail.com>
2024-05-20 13:54:29 +0200

    installed emailjs + made form | name and email bug in form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_i2gfxzr', 'template_eu9wqv2', form.current, {
+        publicKey: 'GZOwweRRjF19AX_XH',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div className='contactPage main'>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+      <div className='contactPage-right'>
+         
+      </div>
+    </div>
+  );
+};
