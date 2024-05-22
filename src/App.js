365366b59abdbd5fd9d594b311523baa109288f0
Epictrepic <laith.benbelgacem@gmail.com>
2024-05-22 11:49:58 +0200

    Added font

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ function App() {
     <>
       <ThemeArea>
         <Helmet>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
+
           <title>Portfolio | Laïs Ben Belgacem</title>
         </Helmet>
         <RouterProvider router={router} />
